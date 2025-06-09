f7dbf62b2c2a2abe4989b9c4558dfc435cade38c
Diego Moreno <dimodelfino@gmail.com>
2025-06-09 17:03:41 -0300

    Assessment solution

--- force-app/main/default/lwc/applicationProcessOfferPresentation/applicationProcessOfferPresentation.js
+++ force-app/main/default/lwc/applicationProcessOfferPresentation/applicationProcessOfferPresentation.js
@@ -1,3 +1,9 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class ApplicationProcessOfferPresentation extends LightningElement {}
+export default class ApplicationProcessOfferPresentation extends LightningElement {
+    @api applicationId;
+    @api totalLoanAmountRequested;
+    @api interestRate;
+    @api monthlyPayment;
+    @api termMonths;
+}
